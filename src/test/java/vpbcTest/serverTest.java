8c0ef211e0f69030561a29b781d4e7c8425c4070
Yehonatan Buchnik <yontyon@gmail.com>
2018-06-24 19:04:19 +0300

    a

--- src/test/java/vpbcTest/serverTest.java
+++ src/test/java/vpbcTest/serverTest.java
@@ -2,6 +2,7 @@ package vpbcTest;
 
 import consensus.vpbc.vpbcServer;
 import org.junit.jupiter.api.Test;
+
 import utiles.SimpleValidator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,6 +10,7 @@ public class serverTest {
     @Test
     void singleServerTest() {
         vpbcServer s = new vpbcServer(0, new SimpleValidator(), "config/testsConfig");
+//        int a = 1;
 //        assertEquals(1, 1);
     }
 
