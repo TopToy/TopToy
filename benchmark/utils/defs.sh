536c631552c5df347e46f7c6703862395d5643be
Yehonatan Buchnik <yontyon@gmail.com>
2019-05-23 09:46:35 +0300

    a

---
+++ benchmark/utils/defs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+gate=toy@13.59.103.171
+base_dir=.
+data_dir=${base_dir}/data
+config_dir=${base_dir}/configurations
+bin_dir=${base_dir}/../bin
+tDir=~/JToy
+user=toy
+n=100
+f=33
+
+
