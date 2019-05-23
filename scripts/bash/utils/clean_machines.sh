35ed42ae9a686cda16d9c1962320b74a2d0c7ae7
Yehonatan Buchnik <yontyon@gmail.com>
2019-05-23 14:50:49 +0300

    remove some files

--- scripts/bash/utils/clean_machines.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source $PWD/definitions.sh
-source $PWD/utils/aws_utils.sh
-
-kill_servers
-kill_clients
--- scripts/bash/utils/watchdog.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source $PWD/definitions.sh
-
-sleep ${1}
-echo "watchdog activated"
-${utils_dir}/clean_machines.sh
